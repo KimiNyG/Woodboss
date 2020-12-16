b3b55029a67faa60fa1da92a6475af52ad40e287
Kimi Nygren <kimi.nygren@student.lab.fi>
2020-12-16 15:50:04 +0200

    some visual changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-
-
   constructor() { }
 
-  ngOnInit() {
-
+  ngOnInit() { 
   }
-
-
 }
